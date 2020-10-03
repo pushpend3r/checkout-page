131601be996e86128a456d47570638bbd7fe5247
Pushpender Singh <piyushthakur5678@gmail.com>
2020-10-03 23:06:28 +0530

    Updates

---
+++ precache-manifest.2d8839ba55e62e944de2633d14452da8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c14cdeb132028a7b3f9fb1396756ab7",
+    "url": "/checkout-page/index.html"
+  },
+  {
+    "revision": "f28a8c222c438336f358",
+    "url": "/checkout-page/static/css/main.907cadcc.chunk.css"
+  },
+  {
+    "revision": "1b899bade93235a4d8b3",
+    "url": "/checkout-page/static/js/2.3e97ba7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/checkout-page/static/js/2.3e97ba7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f28a8c222c438336f358",
+    "url": "/checkout-page/static/js/main.6a95dce6.chunk.js"
+  },
+  {
+    "revision": "fb3b551dd5137949c401",
+    "url": "/checkout-page/static/js/runtime-main.7b849722.js"
+  },
+  {
+    "revision": "66796a87ae608d28b3304955e361b814",
+    "url": "/checkout-page/static/media/account_circle.66796a87.svg"
+  },
+  {
+    "revision": "daa777d7e8a7132d901b95b14f9e0ad5",
+    "url": "/checkout-page/static/media/call.daa777d7.svg"
+  },
+  {
+    "revision": "db3d29e49dad4b7c8392e04fa787f672",
+    "url": "/checkout-page/static/media/email.db3d29e4.svg"
+  },
+  {
+    "revision": "bf30959347d98cf78116456a5980709b",
+    "url": "/checkout-page/static/media/home.bf309593.svg"
+  },
+  {
+    "revision": "cb046bc1ab551020ea9939a9f87304c6",
+    "url": "/checkout-page/static/media/location_city.cb046bc1.svg"
+  },
+  {
+    "revision": "2a0bab29ecbaa77e7d2afd7a6e6b756e",
+    "url": "/checkout-page/static/media/markunread_mailbox.2a0bab29.svg"
+  },
+  {
+    "revision": "4e7df698deee511bfd14f1787f14f376",
+    "url": "/checkout-page/static/media/photo1.4e7df698.png"
+  },
+  {
+    "revision": "44e5172828f6acf7427e432ddc424ec1",
+    "url": "/checkout-page/static/media/photo2.44e51728.png"
+  },
+  {
+    "revision": "315648a54d2ef9e0bbe74454d037ba59",
+    "url": "/checkout-page/static/media/public.315648a5.svg"
+  }
+]);
